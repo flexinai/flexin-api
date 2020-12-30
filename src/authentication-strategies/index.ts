7937300e2f7b2c871c9b7e516fe0aa434f5dc4a0
Tom Monks <monksy@gmail.com>
2020-12-30 06:27:08 -0500

    Authentication code cleanup
    Removed unnecessary code copied from the LB4 example-passport-login project
    
    - express middleware strategies & interceptors
    - session authentication strategy
    - rest-crud implementation

--- src/authentication-strategies/index.ts
+++ src/authentication-strategies/index.ts
@@ -3,11 +3,6 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-// export * from './basic';
-// export * from './facebook';
 export * from './google';
-// export * from './local';
 export * from './oauth2';
-export * from './session';
-// export * from './twitter';
 export * from './types';
