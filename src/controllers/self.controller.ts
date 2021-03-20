bc58268b7153fd65aa89eeedc8e47ba4da1121da
Tom Monks <monksy@gmail.com>
2021-03-20 16:18:39 -0400

    Renamed /whoami to /self

--- src/controllers/self.controller.ts
+++ src/controllers/self.controller.ts
@@ -3,14 +3,10 @@ import {securityId, SecurityBindings, UserProfile} from '@loopback/security';
 import {authenticate, TokenService} from '@loopback/authentication';
 import {get} from '@loopback/rest';
 import {repository} from '@loopback/repository';
-import {
-  TokenServiceBindings,
-  UserServiceBindings,
-  MyUserService,
-} from '@loopback/authentication-jwt';
+import {TokenServiceBindings, UserServiceBindings, MyUserService} from '@loopback/authentication-jwt';
 import {UserRepository} from '../repositories';
 
-export class WhoAmIController {
+export class SelfController {
   constructor(
     @inject(TokenServiceBindings.TOKEN_SERVICE)
     public jwtService: TokenService,
@@ -21,12 +17,10 @@ export class WhoAmIController {
   ) {}
 
   @authenticate('jwt')
-  @get('/whoami')
-  async whoAmI(): Promise<object> {
+  @get('/self')
+  async self(): Promise<object> {
     // return this.user[securityId];
-    const userProfile = await this.userRepository.findById(
-      parseInt(this.user[securityId]),
-    );
+    const userProfile = await this.userRepository.findById(parseInt(this.user[securityId]));
     return userProfile;
   }
 
