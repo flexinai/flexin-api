5a979d7e581aaa5425de647b246e2dd84413e028
Tom Monks <monksy@gmail.com>
2021-08-06 16:21:01 -0400

    Deleted all non-user models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,15 +1,4 @@
-export * from './user.model';
-export * from './exercise.model';
-export * from './intensity.model';
-export * from './program.model';
-export * from './workout.model';
-export * from './set.model';
-export * from './set-statistic.model';
-export * from './category.model';
-export * from './exercise-category.model';
 export * from './user-credentials.model';
 export * from './user-identity.model';
-export * from './clip.model';
-export * from './video.model';
-export * from './workout-video.model';
-export * from './demonstration-video.model';
+export * from './user.model';
+
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,23 +3,15 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {inject, Getter} from '@loopback/core';
+import {Getter, inject} from '@loopback/core';
 import {
   DefaultCrudRepository,
-  HasManyRepositoryFactory,
-  repository,
-  HasOneRepositoryFactory,
+  HasManyRepositoryFactory, HasOneRepositoryFactory, repository
 } from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {User, UserIdentity, UserCredentials, Category, Exercise, Intensity, Program, DemonstrationVideo, WorkoutVideo} from '../models';
-import {UserIdentityRepository} from './user-identity.repository';
+import {User, UserCredentials, UserIdentity} from '../models';
 import {UserCredentialsRepository} from './user-credentials.repository';
-import {CategoryRepository} from './category.repository';
-import {ExerciseRepository} from './exercise.repository';
-import {IntensityRepository} from './intensity.repository';
-import {ProgramRepository} from './program.repository';
-import {DemonstrationVideoRepository} from './demonstration-video.repository';
-import {WorkoutVideoRepository} from './workout-video.repository';
+import {UserIdentityRepository} from './user-identity.repository';
 
 export class UserRepository extends DefaultCrudRepository<
   User,
@@ -35,42 +27,14 @@ export class UserRepository extends DefaultCrudRepository<
     typeof User.prototype.id
   >;
 
-  public readonly categoriesCreated: HasManyRepositoryFactory<Category, typeof User.prototype.id>;
-
-  public readonly exercisesCreated: HasManyRepositoryFactory<Exercise, typeof User.prototype.id>;
-
-  public readonly intensitiesCreated: HasManyRepositoryFactory<Intensity, typeof User.prototype.id>;
-
-  public readonly programsCreated: HasManyRepositoryFactory<Program, typeof User.prototype.id>;
-
-  public readonly programsAssigned: HasManyRepositoryFactory<Program, typeof User.prototype.id>;
-
-  public readonly demonstrationVideos: HasManyRepositoryFactory<DemonstrationVideo, typeof User.prototype.id>;
-
-  public readonly workoutVideos: HasManyRepositoryFactory<WorkoutVideo, typeof User.prototype.id>;
-
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
     @repository.getter('UserIdentityRepository')
     protected profilesGetter: Getter<UserIdentityRepository>,
     @repository.getter('UserCredentialsRepository')
-    protected credentialsGetter: Getter<UserCredentialsRepository>, @repository.getter('CategoryRepository') protected categoryRepositoryGetter: Getter<CategoryRepository>, @repository.getter('ExerciseRepository') protected exerciseRepositoryGetter: Getter<ExerciseRepository>, @repository.getter('IntensityRepository') protected intensityRepositoryGetter: Getter<IntensityRepository>, @repository.getter('ProgramRepository') protected programRepositoryGetter: Getter<ProgramRepository>, @repository.getter('DemonstrationVideoRepository') protected demonstrationVideoRepositoryGetter: Getter<DemonstrationVideoRepository>, @repository.getter('WorkoutVideoRepository') protected workoutVideoRepositoryGetter: Getter<WorkoutVideoRepository>,
+    protected credentialsGetter: Getter<UserCredentialsRepository>
   ) {
     super(User, dataSource);
-    this.workoutVideos = this.createHasManyRepositoryFactoryFor('workoutVideos', workoutVideoRepositoryGetter,);
-    this.registerInclusionResolver('workoutVideos', this.workoutVideos.inclusionResolver);
-    this.demonstrationVideos = this.createHasManyRepositoryFactoryFor('demonstrationVideos', demonstrationVideoRepositoryGetter,);
-    this.registerInclusionResolver('demonstrationVideos', this.demonstrationVideos.inclusionResolver);
-    this.programsAssigned = this.createHasManyRepositoryFactoryFor('programsAssigned', programRepositoryGetter,);
-    this.registerInclusionResolver('programsAssigned', this.programsAssigned.inclusionResolver);
-    this.programsCreated = this.createHasManyRepositoryFactoryFor('programsCreated', programRepositoryGetter,);
-    this.registerInclusionResolver('programsCreated', this.programsCreated.inclusionResolver);
-    this.intensitiesCreated = this.createHasManyRepositoryFactoryFor('intensitiesCreated', intensityRepositoryGetter,);
-    this.registerInclusionResolver('intensitiesCreated', this.intensitiesCreated.inclusionResolver);
-    this.exercisesCreated = this.createHasManyRepositoryFactoryFor('exercisesCreated', exerciseRepositoryGetter,);
-    this.registerInclusionResolver('exercisesCreated', this.exercisesCreated.inclusionResolver);
-    this.categoriesCreated = this.createHasManyRepositoryFactoryFor('categoriesCreated', categoryRepositoryGetter,);
-    this.registerInclusionResolver('categoriesCreated', this.categoriesCreated.inclusionResolver);
     this.profiles = this.createHasManyRepositoryFactoryFor(
       'profiles',
       profilesGetter,
