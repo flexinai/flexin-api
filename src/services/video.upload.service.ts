7ffd165e9f446f5e25f1cf94c25ab2547600a613
Peter Smith <peter@upstateinteractive.io>
2021-12-01 19:09:14 -0800

    feat: upgrade aws sdk

--- src/services/video.upload.service.ts
+++ src/services/video.upload.service.ts
@@ -1,35 +1,21 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-const AWS = require('aws-sdk');
-
-const signedUrlExpireSeconds = 60 * 5;
-const bucket = 'flexin-video';
-
-// read S3 variables from .env
-const config = {
-  region: process.env.S3_REGION,
-  signatureVersion: 'v4',
-  accessKeyId: process.env.S3_ACCESSKEYID,
-  secretAccessKey: process.env.S3_SECRETACCESSKEY,
-  endpoint: process.env.S3_ENDPOINT, // it could be any S3 provider
-};
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class VideoUploadService {
-  s3: any;
 
-  constructor() {
-    this.s3 = new AWS.S3(config);
-  }
-
-  getUploadUrl(fileName: string): object {
-    const uploadParams = {
-      Bucket: bucket,
+  async getUploadUrl(fileName: string): Promise<string> {
+    const client = new S3Client({
+      region: process.env.AWS_DEFAULT_REGION,
+      endpoint: process.env.AWS_ENDPOINT, // it could be any S3 provider
+    });
+    const command = new PutObjectCommand({
+      Bucket: 'flexin-video',
       Key: fileName,
       ContentType: 'application/octet-stream',
-      Expires: signedUrlExpireSeconds,
-    };
+    });
 
-    let url = this.s3.getSignedUrl('putObject', uploadParams);
-    return url;
+    return getSignedUrl(client, command, { expiresIn: 300 });
   }
 }
