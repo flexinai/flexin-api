43fdc604c8b3165feb6761822c586315e7701fa6
Peter Smith <peter@upstateinteractive.io>
2022-01-06 11:22:36 -0500

    get users from auth0

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,53 +1,29 @@
+import {authenticate} from '@loopback/authentication';
 import {
   Count,
   CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
+  Filter, repository,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
   get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
+  getModelSchemaRef, param
 } from '@loopback/rest';
+import {ManagementClient} from 'auth0';
 import {User} from '../models';
 import {UserRepository} from '../repositories';
+
+const management = new ManagementClient({
+  clientId: 'ospPrIAuZWQqMOh3RYDeILRTQR5CSY3i',
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  domain: 'dev-e-vh6xi2.us.auth0.com',
+});
 
 export class UserController {
   constructor(
     @repository(UserRepository)
     public userRepository : UserRepository,
   ) {}
-
-  @post('/users', {
-    responses: {
-      '200': {
-        description: 'User model instance',
-        content: {'application/json': {schema: getModelSchemaRef(User)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {
-            title: 'NewUser',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    user: Omit<User, 'id'>,
-  ): Promise<User> {
-    return this.userRepository.create(user);
-  }
 
   @get('/users/count', {
     responses: {
@@ -63,7 +39,8 @@ export class UserController {
     return this.userRepository.count(where);
   }
 
-  @get('/users', {
+  @authenticate({strategy: 'auth0-jwt', options: {scopes: ['read:users']}})
+  @get('/users/coach', {
     responses: {
       '200': {
         description: 'Array of User model instances',
@@ -80,94 +57,13 @@ export class UserController {
   })
   async find(
     @param.filter(User) filter?: Filter<User>,
-  ): Promise<User[]> {
-    return this.userRepository.find(filter);
-  }
-
-  @patch('/users', {
-    responses: {
-      '200': {
-        description: 'User PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {partial: true}),
-        },
-      },
-    })
-    user: User,
-    @param.where(User) where?: Where<User>,
-  ): Promise<Count> {
-    return this.userRepository.updateAll(user, where);
-  }
-
-  @get('/users/{id}', {
-    responses: {
-      '200': {
-        description: 'User model instance',
-        content: {
-          'application/json': {
-            schema: getModelSchemaRef(User, {includeRelations: true}),
-          },
-        },
-      },
-    },
-  })
-  async findById(
-    @param.path.number('id') id: number,
-    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>
-  ): Promise<User> {
-    return this.userRepository.findById(id, filter);
-  }
-
-  @patch('/users/{id}', {
-    responses: {
-      '204': {
-        description: 'User PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {partial: true}),
-        },
-      },
-    })
-    user: User,
-  ): Promise<void> {
-    await this.userRepository.updateById(id, user);
-  }
-
-  @put('/users/{id}', {
-    responses: {
-      '204': {
-        description: 'User PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.number('id') id: number,
-    @requestBody() user: User,
-  ): Promise<void> {
-    await this.userRepository.replaceById(id, user);
-  }
-
-  @del('/users/{id}', {
-    responses: {
-      '204': {
-        description: 'User DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.number('id') id: number): Promise<void> {
-    await this.userRepository.deleteById(id);
+  ): Promise<void | any[]> {
+    return management
+      .getUsersInRole({
+        id: 'rol_sS8Czj1uRntdt7dF'
+      })
+      .catch((err: unknown) => {
+        console.error(err)
+      });
   }
 }
