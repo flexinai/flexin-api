b0260ae43eb4aa2eae5ba0a3a840ecaf48ec0dc2
Peter Smith <peter@upstateinteractive.io>
2022-02-16 08:40:38 -0800

    feat: update user

--- src/services/video.upload.service.ts
+++ src/services/video.upload.service.ts
@@ -1,12 +1,12 @@
 import {DeleteObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
 import {BindingScope, injectable} from '@loopback/core';
-import {VIDEOTYPES} from '../utils/enums';
+import {UPLOADTYPES} from '../utils/enums';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class VideoUploadService {
 
-  getUploadUrl(fileName: string, path: VIDEOTYPES): Promise<string> {
+  getUploadUrl(fileName: string, path: UPLOADTYPES): Promise<string> {
     const client = new S3Client({
       region: process.env.AWS_DEFAULT_REGION,
     });
