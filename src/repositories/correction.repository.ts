c52bcecf4b9a5f3796e4f57cd04f46c271fdfc46
Peter Smith <peter@upstateinteractive.io>
2022-02-25 12:35:16 -0800

    add reply to correction

--- src/repositories/correction.repository.ts
+++ src/repositories/correction.repository.ts
@@ -1,7 +1,8 @@
 import {Getter, inject} from '@loopback/core';
 import {BelongsToAccessor, DefaultCrudRepository, repository} from '@loopback/repository';
+import {VideoRepository} from '.';
 import {DbDataSource} from '../datasources';
-import {Correction, CorrectionNote, CorrectionRelations} from '../models';
+import {Correction, CorrectionNote, CorrectionRelations, Video} from '../models';
 import {CorrectionNoteRepository} from './correction-note.repository';
 
 export class CorrectionRepository extends DefaultCrudRepository<
@@ -11,12 +12,17 @@ export class CorrectionRepository extends DefaultCrudRepository<
 > {
 
   public readonly correctionNote: BelongsToAccessor<CorrectionNote, typeof Correction.prototype.id>;
+  public readonly reply: BelongsToAccessor<Video, typeof Video.prototype.id>;
 
   constructor(
-    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('CorrectionNoteRepository') protected correctionNoteRepositoryGetter: Getter<CorrectionNoteRepository>,
+    @inject('datasources.db') dataSource: DbDataSource,
+    @repository.getter('CorrectionNoteRepository') protected correctionNoteRepositoryGetter: Getter<CorrectionNoteRepository>,
+    @repository.getter('VideoRepository') protected videoRepositoryGetter: Getter<VideoRepository>,
   ) {
     super(Correction, dataSource);
     this.correctionNote = this.createBelongsToAccessorFor('correctionNote', correctionNoteRepositoryGetter,);
     this.registerInclusionResolver('correctionNote', this.correctionNote.inclusionResolver);
+    this.reply = this.createBelongsToAccessorFor('reply', videoRepositoryGetter,);
+    this.registerInclusionResolver('reply', this.reply.inclusionResolver);
   }
 }
